14cd9f9794608d81523d37f24141fc86a1b61ab5
al0-0la <lorettajjimenez@gmail.com>
2026-08-10 18:37:03 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+TARGET_DOMAIN = "monitor.example.com"
+
+MAX_CONCURRENT = 300
+
+CF_SELECT_DOMAINS = [
+    "cf.tencentapp.cn",
+    "cf.468123.xyz",
+    "cf.090227.xyz",
+    "cf.877774.xyz",
+    "cf.130519.xyz",
+    "cf.008500.xyz",
+    "saas.sin.fan",
+    "cf.3666888.xyz"
+]
+
+CF_OFFICE_DOMAINS = [
+    "icook.hk",
+    "time.is",
+    "staticdelivery.nexusmods.com",
+    "store.ubi.com",
+    "www.shopify.com",
+    "mfa.gov.ua",
+    "www.visa.cn"
+]
